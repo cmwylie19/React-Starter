1299322cf91aadf52610c0381451a7124642063f
Case Wylie <cwylie@redhat.com>
2019-09-25 15:39:08 -0400

    snapshot test

--- src/containers/AppContainer.test.js
+++ src/containers/AppContainer.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './AppContainer';
+import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AppContainer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders correctly', () => {
+    const tree = renderer
+      .create(<AppContainer />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
